c58a45e53ec5c1effb154df1dcf65df3b641378d
Vanitha Seeralan <87183209+Vanithaseeralan1992@users.noreply.github.com>
2021-07-11 14:56:19 +0200

    Add files via upload

---
+++ my_first_test.js
@@ -0,0 +1,36 @@
+///<reference types="cypress"/>
+
+it('Google test', function(){
+cy.visit('https://www.google.com/')
+cy.get('#L2AGLb > .jyfHyd').click()
+cy.get('.gLFyf').type('Automation step by step{enter}')
+//cy.visit('https://www.altinn.no/')
+})
+
+it('Altinn test', function(){
+cy.visit('https://accounts.google.com/')
+cy.wait(2000)
+cy.get('body').type('vanithaseeralan@gmail.com')
+//cy.wait(1000)
+cy.get('#identifierNext > .VfPpkd-dgl2Hf-ppHlrf-sM5MNb > .VfPpkd-LgbsSe > .VfPpkd-vQzf8d').click().wait(1000)
+//cy.wait(1000)
+
+})
+
+it('facebook login', function(){
+    cy.visit('https://www.facebook.com').wait(1000)
+    cy.get('[data-testid=cookie-policy-dialog-accept-button]').click()
+
+    cy.get('[data-testid=royal_email]').type('vani000000@gmail.com').wait(1000)
+    cy.get('[data-testid=royal_pass]').type('********').wait(1000)
+    cy.get('[data-testid=royal_login_button]').click()
+
+})
+
+it.only('facebook signup automation', function()
+{
+ cy.visit('https://www.facebook.com/campaign/landing.php')
+ cy.get('[data-testid=cookie-policy-dialog-accept-button]').click()
+
+
+})
